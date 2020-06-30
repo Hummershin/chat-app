e98aa3b35b9b0f3e1afc3d79a720ab88dd4afc61
Hummershin <64775577+Hummershin@users.noreply.github.com>
2020-06-30 10:10:27 +0900

    room_usersテーブルの作成

--- db/migrate/20200630010455_create_room_users.rb
+++ db/migrate/20200630010455_create_room_users.rb
@@ -1,8 +1,7 @@
 class CreateRoomUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :room_users do |t|
-      t.integer :room_id, foreign_key: true
-      t.integer :user_id, foreign_key: true
+
       t.timestamps
     end
   end
